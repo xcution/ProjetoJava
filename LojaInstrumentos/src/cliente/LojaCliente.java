4c8bc120dcd96b820284650b8e2f04515ddc2336
luizverissimo <luizverissimoccr@hotmail.com>
2015-11-06 19:16:57 -0200

    modificação metodo autentica e internalframes

---
+++ LojaInstrumentos/src/cliente/LojaCliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loja.cliente;
+
+/**
+ *
+ * @author Xcution
+ */
+public class LojaCliente {
+    public LojaCliente(){}
+    public boolean CadastrarUsuario(){return true;}
+    public boolean AtualizarUsuario(){return true;}
+    public boolean RemoverUsuario(){return true;}
+    public boolean ConsultarUsuario(){return true;}
+            
+}
